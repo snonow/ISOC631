c0ddd1b85e2757c43eaea0d390db8e6523f08827
skiiing73 <corentindegouey@gmail.com>
2024-04-17 15:06:12 +0200

    modif config

--- hardhat/contracts/test_contrat_arno.sol
+++ hardhat/contracts/test_contrat_arno.sol
@@ -1,103 +1,116 @@
-pragma solidity ^0.8.19;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 contract AxelrodGame {
-    // Variables d'état
-    uint public constant BET_MIN = 1; // Mise minimale
-    uint public constant REVEAL_TIMEOUT = 600; // 10 minutes en secondes
     address public player1;
     address public player2;
-    bytes32 public hashedMovePlayer1; 
-    bytes32 public hashedMovePlayer2;
-    uint public betAmountPlayer1;
-    uint public betAmountPlayer2;
-    uint public revealDeadline;
-    uint public movePlayer1;
-    uint public movePlayer2;
+    uint256 public betMin;
+    uint256 public revealTimeout;
+    uint256 public initialBet;
 
-    // Modificateurs
-    modifier onlyRegisteredPlayer() {
-        require(msg.sender == player1 || msg.sender == player2, "You are not a registered player.");
-        _;
+    enum Move {Cooperate, Defect}
+    struct Player {
+        bytes32 moveHash;
+        bool revealed;
+        uint256 bet;
     }
 
-    modifier onlyBeforeRevealDeadline() {
-        require(block.timestamp < revealDeadline, "Reveal deadline has passed.");
-        _;
+    mapping(address => Player) public players;
+
+    event GameStarted(address player1, address player2, uint256 bet);
+    event MoveSubmitted(address player);
+    event GameEnded(address winner, uint256 winnings);
+
+    constructor(uint256 _betMin, uint256 _revealTimeout) {
+        betMin = _betMin;
+        revealTimeout = _revealTimeout;
     }
 
-    // Fonction d'enregistrement des joueurs
     function register() external payable {
-        require(msg.value >= BET_MIN, "Insufficient bet amount.");
+        require(player1 == address(0) || player2 == address(0), "Game is full");
+        require(msg.value >= betMin, "Insufficient bet");
+        
         if (player1 == address(0)) {
             player1 = msg.sender;
-            betAmountPlayer1 = msg.value;
-        } else if (player2 == address(0)) {
-            require(msg.value > betAmountPlayer1, "Bet amount must be greater than player1's bet.");
+            initialBet = msg.value;
+        } else {
             player2 = msg.sender;
-            betAmountPlayer2 = msg.value;
-            revealDeadline = block.timestamp + REVEAL_TIMEOUT;
+            initialBet = players[player1].bet;
+            require(msg.value >= initialBet, "Bet must be equal or higher than initial player's bet");
+        }
+
+        players[msg.sender].bet = msg.value;
+        if (player1 != address(0) && player2 != address(0)) {
+            emit GameStarted(player1, player2, initialBet);
         }
     }
 
-    // Fonction de placement du coup (hashé)
-    function commitMove(bytes32 _hashedMove) external onlyRegisteredPlayer {
-        require(block.timestamp < revealDeadline, "Reveal deadline has passed.");
-        if (msg.sender == player1) {
-            hashedMovePlayer1 = _hashedMove;
+    function submitMove(bytes32 _moveHash) external {
+        require(players[msg.sender].moveHash == bytes32(0), "Move already submitted");
+        players[msg.sender].moveHash = _moveHash;
+        emit MoveSubmitted(msg.sender);
+
+        if (players[player1].moveHash != bytes32(0) && players[player2].moveHash != bytes32(0)) {
+            revealMoves();
+        }
+    }
+
+    function revealMove(bytes32 _password) external {
+        Player storage player = players[msg.sender];
+        require(player.moveHash != bytes32(0), "Move not submitted");
+        require(!player.revealed, "Move already revealed");
+        require(keccak256(abi.encodePacked(_password)) == player.moveHash, "Invalid password");
+
+        player.revealed = true;
+        if (players[player1].revealed && players[player2].revealed) {
+            revealMoves();
+        }
+    }
+
+    function revealMoves() private {
+        Player storage player1Data = players[player1];
+        Player storage player2Data = players[player2];
+
+        bytes32 player1MoveHash = player1Data.moveHash;
+        bytes32 player2MoveHash = player2Data.moveHash;
+
+        require(player1MoveHash != bytes32(0) && player2MoveHash != bytes32(0), "Moves not submitted");
+
+        Move player1Move = Move(uint8(uint256(keccak256(abi.encodePacked(player1MoveHash, address(this)))) % 2));
+        Move player2Move = Move(uint8(uint256(keccak256(abi.encodePacked(player2MoveHash, address(this)))) % 2));
+
+        if (player1Move == Move.Cooperate && player2Move == Move.Cooperate) {
+            payOut(player1, player2, initialBet, initialBet);
+        } else if (player1Move == Move.Cooperate && player2Move == Move.Defect) {
+            payOut(player2, player1, initialBet * 2, 0);
+        } else if (player1Move == Move.Defect && player2Move == Move.Cooperate) {
+            payOut(player1, player2, 0, initialBet * 2);
         } else {
-            hashedMovePlayer2 = _hashedMove;
+            payOut(address(0), address(0), 0, 0); // Both defect, no winner
         }
     }
 
-    // Fonction de révélation du coup
-    function revealMove(uint _move, string memory _password) external onlyRegisteredPlayer onlyBeforeRevealDeadline {
-        bytes32 hashedMove;
-        if (msg.sender == player1) {
-            hashedMove = keccak256(abi.encodePacked(_move, _password));
-            require(hashedMove == hashedMovePlayer1, "Invalid move or password.");
-            movePlayer1 = _move;
-        } else {
-            hashedMove = keccak256(abi.encodePacked(_move, _password));
-            require(hashedMove == hashedMovePlayer2, "Invalid move or password.");
-            movePlayer2 = _move;
+    function payOut(address _winner, address _loser, uint256 _winnerAmount, uint256 _loserAmount) private {
+        if (_winner != address(0)) {
+            payable(_winner).transfer(_winnerAmount);
+            emit GameEnded(_winner, _winnerAmount);
         }
-        if (movePlayer1 != 0 && movePlayer2 != 0) {
-            determineWinner();
+        if (_loser != address(0)) {
+            payable(_loser).transfer(_loserAmount);
+            emit GameEnded(_loser, _loserAmount);
         }
-    }
-
-    // Fonction de détermination du gagnant et distribution des récompenses
-    function determineWinner() private {
-        if (movePlayer1 == movePlayer2) {
-            payable(player1).transfer(betAmountPlayer1);
-            payable(player2).transfer(betAmountPlayer2);
-        } else if (movePlayer1 == 1 && movePlayer2 == 2) {
-            payable(player2).transfer(betAmountPlayer1 + betAmountPlayer2);
-        } else if (movePlayer1 == 2 && movePlayer2 == 1) {
-            payable(player1).transfer(betAmountPlayer1 + betAmountPlayer2);
-        } else if (movePlayer1 == 1 && movePlayer2 == 0) {
-            payable(player1).transfer(betAmountPlayer1 + betAmountPlayer2);
-        } else if (movePlayer1 == 0 && movePlayer2 == 1) {
-            payable(player2).transfer(betAmountPlayer1 + betAmountPlayer2);
-        } else if (movePlayer1 == 2 && movePlayer2 == 0) {
-            payable(player2).transfer(betAmountPlayer1 + betAmountPlayer2);
-        } else if (movePlayer1 == 0 && movePlayer2 == 2) {
-            payable(player1).transfer(betAmountPlayer1 + betAmountPlayer2);
-        }
-        // Réinitialisation de l'état du jeu
         resetGame();
     }
 
-    // Fonction de réinitialisation du jeu
     function resetGame() private {
         player1 = address(0);
         player2 = address(0);
-        hashedMovePlayer1 = 0;
-        hashedMovePlayer2 = 0;
-        betAmountPlayer1 = 0;
-        betAmountPlayer2 = 0;
-        revealDeadline = 0;
-        movePlayer1 = 0;
-        movePlayer2 = 0;
+        initialBet = 0;
+        delete players[player1];
+        delete players[player2];
+    }
+
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 }
