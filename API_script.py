9320e9b5080264aa5b27e684045ccf75424982db
elementaryy <elementary38360@gmail.com>
2024-05-16 10:01:38 +0200

    super toto repare tout #senior

--- API_script.py
+++ API_script.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
 from sympy import false
 from web3 import Web3
+from config_api import contract_abi, contract_address
 
 
 app = FastAPI()
@@ -15,256 +16,18 @@ class MoveSubmission(BaseModel):
     player: str
     moveHash: str
 
-
-# Adresse du contrat et ABI (Application Binary Interface) 
-
-###############################################################
-#    modifié avec l'adresse du contrat une fois compilé
-contract_address = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
-###############################################################
-
-
-
-
-contract_abi = [
-    {
-				"inputs": [
-					{
-						"internalType": "uint256",
-						"name": "_betMin",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "_revealTimeout",
-						"type": "uint256"
-					}
-				],
-				"stateMutability": "nonpayable",
-				"type": "constructor"
-			},
-			{
-				"anonymous": false,
-				"inputs": [
-					{
-						"indexed": false,
-						"internalType": "address",
-						"name": "winner",
-						"type": "address"
-					},
-					{
-						"indexed": false,
-						"internalType": "uint256",
-						"name": "winnings",
-						"type": "uint256"
-					}
-				],
-				"name": "GameEnded",
-				"type": "event"
-			},
-			{
-				"anonymous": false,
-				"inputs": [
-					{
-						"indexed": false,
-						"internalType": "address",
-						"name": "player1",
-						"type": "address"
-					},
-					{
-						"indexed": false,
-						"internalType": "address",
-						"name": "player2",
-						"type": "address"
-					},
-					{
-						"indexed": false,
-						"internalType": "uint256",
-						"name": "bet",
-						"type": "uint256"
-					}
-				],
-				"name": "GameStarted",
-				"type": "event"
-			},
-			{
-				"anonymous": false,
-				"inputs": [
-					{
-						"indexed": false,
-						"internalType": "address",
-						"name": "player",
-						"type": "address"
-					}
-				],
-				"name": "MoveSubmitted",
-				"type": "event"
-			},
-			{
-				"inputs": [],
-				"name": "betMin",
-				"outputs": [
-					{
-						"internalType": "uint256",
-						"name": "",
-						"type": "uint256"
-					}
-				],
-				"stateMutability": "view",
-				"type": "function"
-			},
-			{
-				"inputs": [],
-				"name": "getContractBalance",
-				"outputs": [
-					{
-						"internalType": "uint256",
-						"name": "",
-						"type": "uint256"
-					}
-				],
-				"stateMutability": "view",
-				"type": "function"
-			},
-			{
-				"inputs": [],
-				"name": "initialBet",
-				"outputs": [
-					{
-						"internalType": "uint256",
-						"name": "",
-						"type": "uint256"
-					}
-				],
-				"stateMutability": "view",
-				"type": "function"
-			},
-			{
-				"inputs": [],
-				"name": "player1",
-				"outputs": [
-					{
-						"internalType": "address",
-						"name": "",
-						"type": "address"
-					}
-				],
-				"stateMutability": "view",
-				"type": "function"
-			},
-			{
-				"inputs": [],
-				"name": "player2",
-				"outputs": [
-					{
-						"internalType": "address",
-						"name": "",
-						"type": "address"
-					}
-				],
-				"stateMutability": "view",
-				"type": "function"
-			},
-			{
-				"inputs": [
-					{
-						"internalType": "address",
-						"name": "",
-						"type": "address"
-					}
-				],
-				"name": "players",
-				"outputs": [
-					{
-						"internalType": "bytes32",
-						"name": "moveHash",
-						"type": "bytes32"
-					},
-					{
-						"internalType": "bool",
-						"name": "revealed",
-						"type": "bool"
-					},
-					{
-						"internalType": "uint256",
-						"name": "bet",
-						"type": "uint256"
-					}
-				],
-				"stateMutability": "view",
-				"type": "function"
-			},
-			{
-				"inputs": [],
-				"name": "register",
-				"outputs": [],
-				"stateMutability": "payable",
-				"type": "function"
-			},
-			{
-				"inputs": [
-					{
-						"internalType": "bytes32",
-						"name": "_password",
-						"type": "bytes32"
-					}
-				],
-				"name": "revealMove",
-				"outputs": [],
-				"stateMutability": "nonpayable",
-				"type": "function"
-			},
-			{
-				"inputs": [],
-				"name": "revealTimeout",
-				"outputs": [
-					{
-						"internalType": "uint256",
-						"name": "",
-						"type": "uint256"
-					}
-				],
-				"stateMutability": "view",
-				"type": "function"
-			},
-			{
-				"inputs": [
-					{
-						"internalType": "bytes32",
-						"name": "_moveHash",
-						"type": "bytes32"
-					}
-				],
-				"name": "submitMove",
-				"outputs": [],
-				"stateMutability": "nonpayable",
-				"type": "function"
-			}
-	
-    
-]
-
 # Chargement du contrat
 contract = w3.eth.contract(address=contract_address, abi=contract_abi)
 
 
 # Endpoint pour enregistrer un joueur
-@app.get("/register")
+@app.get("/register/")
 async def register(player_address: str = Query(..., title="Player Address", description="The address of the player"),
                    bet_amount: int = Query(..., title="Bet Amount", description="The amount of the bet in wei")):
     try:
     
-
-
-        # Vérifier si le montant de la mise est suffisant
-        if bet_amount < contract.functions.betMin().call():
-            raise ValueError("Bet amount is less than minimum bet required")
-
         # Enregistrer le joueur en appelant la fonction register du contrat Ethereum
-        tx_hash = contract.functions.register().transact({'from': player_address, 'value': bet_amount})
-
-        # Attendre la confirmation de la transaction
-        w3.eth.waitForTransactionReceipt(tx_hash)
+        contract.functions.register().transact({'from': player_address, 'value': bet_amount})
 
         return {"message": "Player registered successfully"}
     
@@ -273,8 +36,42 @@ async def register(player_address: str = Query(..., title="Player Address", desc
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
+# Définir les endpoints de votre API
+@app.get("/avoir_movehash/")
+async def avoir_movehash(password: str = Query(...)):
+    # Appeler la fonction avoir_movehash de votre contrat Solidity
+    try:
+        result_bytes32 = contract.functions.avoir_movehash(password).call()
+        # Convertir bytes32 en hexadécimal
+        result_hex = Web3.toHex(result_bytes32)
+        return {"moveHash": result_hex}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+# Définir les endpoints de votre API
+@app.get("/both_revealed/")
+async def both_revealed():
+    # Appeler la fonction bothRevealed de votre contrat Solidity
+    try:
+        result = contract.functions.bothRevealed().call()
+        return {"bothRevealed": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Définir les endpoints de votre API
+@app.get("/who_am_i/")
+async def who_am_i():
+    # Appeler la fonction whoAmI de votre contrat Solidity
+    try:
+        result = contract.functions.whoAmI().call()
+        return {"playerId": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
 # Point d'entrée pour exécuter le serveur
+
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ config_api.py
@@ -0,0 +1,158 @@
+contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+contract_abi = [
+    {
+        "inputs": [],
+        "stateMutability": "payable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "register",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "bytes32",
+                "name": "_moveHash",
+                "type": "bytes32"
+            }
+        ],
+        "name": "submitMove",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_password",
+                "type": "string"
+            }
+        ],
+        "name": "revealMove",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getOutcome",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getContractBalance",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "whoAmI",
+        "outputs": [
+            {
+                "internalType": "uint8",
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "bothPlayed",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "bothRevealed",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "revealTimeLeft",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_password",
+                "type": "string"
+            }
+        ],
+        "name": "avoir_movehash",
+        "outputs": [
+            {
+                "internalType": "bytes32",
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "stateMutability": "pure",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "player1",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "player2",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
